7833f56c45e6cf6cb5a600ed37e400e0d23bcd1e
Ananda Dwi Prayoga <prayogaananda04@gmail.com>
2022-02-25 20:52:40 +0700

    finish add widget

---
+++ app/src/main/java/com/pasukanlangit/id/customview/NumberGenerator.kt
@@ -0,0 +1,11 @@
+package com.pasukanlangit.id.customview
+
+import java.util.*
+
+
+internal object NumberGenerator {
+    fun generate(max: Int): Int{
+        val random = Random()
+        return random.nextInt(max)
+    }
+}
