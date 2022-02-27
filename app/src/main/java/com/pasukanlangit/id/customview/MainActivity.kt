7459f89f7cea31373e93cffb0d5674dcbf67392c
Ananda Dwi Prayoga <prayogaananda04@gmail.com>
2022-02-27 19:55:19 +0700

    finish webview

--- app/src/main/java/com/pasukanlangit/id/customview/MainActivity.kt
+++ app/src/main/java/com/pasukanlangit/id/customview/MainActivity.kt
@@ -1,108 +1,50 @@
 package com.pasukanlangit.id.customview
 
-import android.graphics.*
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.Window
+import android.webkit.JsResult
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.res.ResourcesCompat
 import com.pasukanlangit.id.customview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(){
     private lateinit var binding: ActivityMainBinding
 
-    private val mBitmap = Bitmap.createBitmap(1000,1000, Bitmap.Config.ARGB_8888)
-    private val mCanvas = Canvas(mBitmap)
-    private val mPaint = Paint()
-
-    private val halfOfWidth = (mBitmap.width/2).toFloat()
-    private val halfOfHeight = (mBitmap.height/2).toFloat()
-
-    private val left = 150f
-    private val top = 250f
-    private val right = mBitmap.width - left
-    private val bottom = mBitmap.height.toFloat() - 50f
-
-    private val message = "Apakah kamu suka bermain?"
-
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        supportActionBar?.hide()
         setContentView(binding.root)
 
-        binding.imageView.setImageBitmap(mBitmap)
-        binding.like.setOnClickListener {
-            buildFace(isFaceHappy = true)
-        }
-        binding.dislike.setOnClickListener {
-            buildFace(isFaceHappy = false)
-        }
+        binding.webView.apply {
+            settings.javaScriptEnabled = true
 
-        showText()
-    }
-
-    private fun showText() {
-        val mPainText = Paint(Paint.FAKE_BOLD_TEXT_FLAG).apply {
-            textSize = 50f
-            color = ResourcesCompat.getColor(resources, R.color.black, null)
-        }
-
-        val mBounds = Rect()
-        mPainText.getTextBounds(message, 0, message.length, mBounds)
-
-        val x: Float = halfOfWidth - mBounds.centerX()
-        val y = 50f
-        mCanvas.drawText(message, x, y, mPainText)
-    }
-
-    private fun buildFace(isFaceHappy: Boolean) {
-        showFace()
-        showEyes()
-        showMouth(isFaceHappy)
-    }
-
-    private fun showMouth(isHappy: Boolean) {
-        when(isHappy){
-            true -> {
-                mPaint.color = ResourcesCompat.getColor(resources, R.color.black, null)
-                val lip = RectF(halfOfWidth-200f, halfOfHeight-100f,halfOfWidth+200f, halfOfHeight+400f)
-                mCanvas.drawArc(lip,25f,130f,false,mPaint)
-
-                mPaint.color = ResourcesCompat.getColor(resources, R.color.white, null)
-                val mouth = RectF(halfOfWidth - 180F, halfOfHeight, halfOfWidth + 180F, halfOfHeight + 380F)
-                mCanvas.drawArc(mouth, 25F, 130F, false, mPaint)            }
-            false -> {
-                mPaint.color = ResourcesCompat.getColor(resources, R.color.black, null)
-                val lip = RectF(halfOfWidth - 200F, halfOfHeight + 250F, halfOfWidth + 200F, halfOfHeight + 350F)
-                mCanvas.drawArc(lip, 0F, -180F, false, mPaint)
-
-                mPaint.color = ResourcesCompat.getColor(resources, R.color.white, null)
-                val mouth = RectF(halfOfWidth - 180F, halfOfHeight + 260F, halfOfWidth + 180F, halfOfHeight + 330F)
-                mCanvas.drawArc(mouth, 0F, -180F, false, mPaint)
+            webViewClient = object : WebViewClient(){
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    Toast.makeText(this@MainActivity, "Finish load page", Toast.LENGTH_SHORT).show()
+                }
             }
+            webChromeClient = object: WebChromeClient(){
+                override fun onJsAlert(
+                    view: WebView?,
+                    url: String?,
+                    message: String?,
+                    result: JsResult?
+                ): Boolean {
+                    Toast.makeText(this@MainActivity, message,Toast.LENGTH_LONG).show()
+                    result?.confirm()
+                    return true
+                }
+            }
+
+            loadUrl("https://www.dicoding.com")
+
         }
+
+
     }
-
-    private fun showEyes() {
-        mPaint.color = ResourcesCompat.getColor(resources, R.color.black, null)
-        mCanvas.drawCircle(halfOfWidth-100f, halfOfHeight-10f, 50f, mPaint)
-        mCanvas.drawCircle(halfOfWidth+100f, halfOfHeight-10f,50f, mPaint)
-
-        mPaint.color = ResourcesCompat.getColor(resources, R.color.white, null)
-        mCanvas.drawCircle(halfOfWidth-120f, halfOfHeight-20f, 15f, mPaint)
-        mCanvas.drawCircle(halfOfWidth+80f, halfOfHeight-20f, 15f, mPaint)
-    }
-
-    private fun showFace(){
-        val face = RectF(left, top, right, bottom)
-
-        mPaint.color = ResourcesCompat.getColor(resources, R.color.yellow_left_skin, null)
-        mCanvas.drawArc(face,90f,180f,false,mPaint)
-
-        mPaint.color = ResourcesCompat.getColor(resources, R.color.yellow_right_skin, null)
-        mCanvas.drawArc(face, 270F, 180F, false, mPaint)
-    }
-
 
 }
